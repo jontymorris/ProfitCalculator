0de140f3c1cc0de25644b33921638a3cb098de47
Jonty <jonty.morris@yahoo.co.nz>
2019-06-03 21:05:02 +1200

    Added sales functionality

--- app/src/main/java/me/morris/grossprofitcalculator/SalesActivity.kt
+++ app/src/main/java/me/morris/grossprofitcalculator/SalesActivity.kt
@@ -3,8 +3,12 @@ package me.morris.grossprofitcalculator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class SalesActivity : AppCompatActivity() {
+
+    private var utils: AppUtils? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +16,8 @@ class SalesActivity : AppCompatActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        utils = AppUtils(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -20,7 +26,22 @@ class SalesActivity : AppCompatActivity() {
     }
 
     fun calculate(view: View) {
+        var price = utils!!.getDouble(R.id.priceEdit)
+        var sale = utils!!.getDouble(R.id.saleEdit)
+        var saved = price
 
+        utils!!.clearEdit(R.id.priceEdit)
+        utils!!.clearEdit(R.id.saleEdit)
+
+        price *= 1 - (sale/100)
+        saved -= price
+
+        utils!!.setTextValue(R.id.savedText, "$" + utils!!.roundValue(saved))
+        utils!!.setTextValue(R.id.saleText, utils!!.roundValue(sale) + "%")
+        utils!!.setTextValue(R.id.priceText, "$" + utils!!.roundValue(price))
+
+        utils!!.showView(R.id.infoLayout)
+        utils!!.hideKeyboard(view)
     }
 
 }

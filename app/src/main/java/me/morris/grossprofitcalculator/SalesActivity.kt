2018362172699e896ebc762ebc342ee0811de96f
Jonty <jonty.morris@yahoo.co.nz>
2019-06-03 20:11:06 +1200

    Added back button

--- app/src/main/java/me/morris/grossprofitcalculator/SalesActivity.kt
+++ app/src/main/java/me/morris/grossprofitcalculator/SalesActivity.kt
@@ -9,6 +9,14 @@ class SalesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sales)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     fun calculate(view: View) {
